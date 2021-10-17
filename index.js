83723a96f7fd3d229821bed17881721117ed7896
jamahadrummer <jamahadrummer@gmail.com>
2021-10-17 17:30:01 -0500

    page simplification

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 console.log('Start Script')
+let headerLeft = document.getElementsByClassName('headerLeft')
 
-
-
-
-
-
-
-
-
-
-
+const pageTurn = (pageName) => {
+    location.href = pageName;
+}
 
 console.log('End Script')
