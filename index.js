96af67cda52f7d4e75f5b027f15c1595c7ef5698
jamahadrummer <jamahadrummer@live.com>
2021-10-20 14:44:16 -0500

    button link js

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
-console.log('Start Script')
-let headerLeft = document.getElementsByClassName('headerLeft')
+console.log('Start Script');
+const headerLeftBtn = document.getElementById('headerLeftBtn');
+const content2btn1 = document.getElementById('content2btn1');
+const content2btn2 = document.getElementById('content2btn2');
+const content2btn3 = document.getElementById('content2btn3');
+const content2btn4 = document.getElementById('content2btn4');
 
 const pageTurn = (pageName) => {
-    location.href = pageName;
-}
+	location.href = pageName;
+};
 
-console.log('End Script')
+headerLeftBtn.addEventListener('click', () => {
+	pageTurn('index.html');
+});
+
+content2btn1.addEventListener('click', () => {
+	pageTurn('https://www.flickr.com/jamahadrummer');
+});
+
+content2btn2.addEventListener('click', () => {
+	pageTurn('https://wwww.instagram.com/jamahadrummer');
+});
+
+content2btn3.addEventListener('click', () => {
+	pageTurn('https://jamadaniel.wixsite.com/sound-designer');
+});
+
+content2btn4.addEventListener('click', () => {
+	pageTurn('https://github.com/jamahadrummer');
+});
+
+console.log('End Script');
